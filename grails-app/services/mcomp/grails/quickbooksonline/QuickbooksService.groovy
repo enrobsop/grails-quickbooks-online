d8a655bd690823f69cba2d6cadd10463db5d4906
Paul Osborne <hello@paulosborne.me.uk>
2014-01-21 14:58:26 +0000

    Added Quickbooks service and method to get response as JSON.

---
+++ grails-app/services/mcomp/grails/quickbooksonline/QuickbooksService.groovy
@@ -0,0 +1,19 @@
+package mcomp.grails.quickbooksonline
+import org.scribe.model.Response
+import org.scribe.model.Token
+import uk.co.desirableobjects.oauth.scribe.OauthService
+
+class QuickbooksService {
+
+	OauthService oauthService
+
+	String getTokenSessionKey() {
+		oauthService.findSessionKeyForAccessToken('intuit')
+	}
+
+	Response getJsonResponse(Token token, String url, Map<String,String> querystringParams) {
+		def extraHeaders = [Accept:"application/json", "Content-Type":"application/json"]
+		oauthService.getIntuitResourceWithQuerystringParams(token, url, querystringParams, extraHeaders)
+	}
+
+}
---
+++ test/unit/mcomp/grails/quickbooksonline/QuickbooksServiceSpec.groovy
@@ -0,0 +1,56 @@
+package mcomp.grails.quickbooksonline
+
+import grails.plugin.spock.UnitSpec
+import grails.test.mixin.TestFor
+import org.scribe.model.Response
+import org.scribe.model.Token
+import uk.co.desirableobjects.oauth.scribe.OauthService
+
+@TestFor(QuickbooksService)
+class QuickbooksServiceSpec extends UnitSpec {
+
+	def "user can get the session key containing the access token"() {
+
+		given: "args"
+			OauthService oauthService = Mock(OauthService)
+			service.oauthService = oauthService
+
+		when:
+			def result = service.tokenSessionKey
+
+		then: "the service delegates correctly"
+			1 * oauthService.findSessionKeyForAccessToken("intuit") >> "theKey"
+		and: "the correct key is given"
+			result == "theKey"
+
+	}
+
+	def "user can execute a request and get results as JSON"() {
+
+		given: "args"
+			def theUrl = "https://qb.sbfinance.intuit.com/v3/company/1234567890/query"
+			def theQuerystringParms = [query: "SELECT Id, DisplayName, Active, Balance FROM Customer"]
+		and: "an access token"
+			def theToken = new Token("token","secret")
+		and: "a service"
+			OauthService oauthService = Mock(OauthService)
+			service.oauthService = oauthService
+		and: "the expected response"
+			def expectedResponse = Mock(Response)
+
+		when:
+			Response response = service.getJsonResponse(theToken, theUrl, theQuerystringParms)
+
+		then: "the plugin delegates correctly"
+			1 * oauthService.methodMissing('getIntuitResourceWithQuerystringParams', [
+					theToken,
+					theUrl,
+					theQuerystringParms,
+					[Accept:"application/json", "Content-Type":"application/json"]
+			]) >> expectedResponse
+		and: "the correct response is returned"
+			response == expectedResponse
+
+	}
+
+}
