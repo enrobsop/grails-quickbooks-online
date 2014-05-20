136f2b73cbed17e92fe54d3ce6d550fe46dc9a74
Paul Osborne <hello@paulosborne.me.uk>
2014-05-20 15:24:48 +0100

    Updated OAuth plugin dependency now that required new features of v2.5 are available on the maven repo.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,10 +10,12 @@ grails.project.dependency.resolution = {
 	repositories {
 		grailsCentral()
 		mavenCentral()
+		mavenRepo "http://repo.desirableobjects.co.uk/"
+		mavenRepo 'https://raw.github.com/fernandezpablo85/scribe-java/mvn-repo'
 	}
 
 	dependencies {
-		compile 'org.scribe:scribe:1.3.5'
+		compile 'org.scribe:scribe:1.3.6'
 		test('org.spockframework:spock-grails-support:0.7-groovy-2.0',
 				"org.objenesis:objenesis:1.2") {
 			export = false
@@ -32,9 +34,9 @@ grails.project.dependency.resolution = {
 			exclude 'spock-grails-support'
 		}
 
-		compile(":grails-oauth:2.4-SNAPSHOT") {
+		compile(":oauth:2.5") {
 			excludes 'spock', 'spock-core', 'objenesis','gmock'
-		}   // local zip
+		}
 
 	}
 
